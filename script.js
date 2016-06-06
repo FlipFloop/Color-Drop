5e4f194e3e718fadca95107e3d5daba9feb892c5
FlipFloop <flipfloopgames@gmail.com>
2016-06-06 21:07:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   
   $('#options').change(function(){ // NOTE: all themes have capital letters on colors
     if($(this).val() === 'Dark'){ 
-      $("#game").css("background-color", "#000");
+      $("#game").css("background-color", "black");
       $("#player").css({
         "background-color": "white" // using CSS function in case you want to add other stuff
       });
