5140cc201fe70e3040f768f17d090b27bd820601
FlipFloop <flipfloopgames@gmail.com>
2016-06-06 20:39:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ $(document).ready(function() {
   $("#play").click(function() {
     $(this).hide();
     $("#options").hide();
+    $("#player").show();
   });
   
-  $("#player").show();
+  $('#options').change(function(){
+    if($(this).val() == 'dark'){ 
+      $("#game").css("backgroundColor", "#000000");
+    }
+  });
   
 });
