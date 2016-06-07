ce414327c593f9679aa7a2e13c62ec590c394f90
FlipFloop <flipfloopgames@gmail.com>
2016-06-07 22:16:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 $(document).ready(function() {
   
-  //$('#options').change(function(){ // NOTE: all themes have capital letters on colors
-    if($("#options").val() === 'Dark'){ 
+  $('#options').change(function(){ // NOTE: all themes have capital letters on colors
+    if($(this).val() === 'Dark'){ 
       $("#game").css("background-color", "black");
       $("#player").css({
         "background-color": "white" // using CSS function in case you want to add other stuff
       });
     }
-  //});
+    if($(this).val() === 'Light'){ 
+      $("#game").css("background-color", "white");
+      $("#player").css({
+        "background-color": "black" // using CSS function in case you want to add other stuff
+      });
+    }
+  });
   
   $("#play").click(function() {
     $(this).hide();
