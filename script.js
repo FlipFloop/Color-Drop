2580c0fefc90a44b960d2c283f25fe9ace4e96b0
FlipFloop <flipfloopgames@gmail.com>
2016-06-11 20:21:29 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
-  
+
   $('#options').change(function(){ // NOTE: all themes have capital letters on colors
-    if($(this).val() === 'Dark'){ 
+    if($(this).val() === 'Dark'){
       $("#game").css("background-color", "black");
       $("#player").css({
         "background-color": "white" // using CSS function in case you want to add other stuff
       });
     }
-    if($(this).val() === 'Light'){ 
+    if($(this).val() === 'Light'){
       $("#game").css("background-color", "white");
       $("#player").css({
         "background-color": "black" // using CSS function in case you want to add other stuff
       });
     }
   });
-  
+
   $("#play").click(function() {
-    if($("#options").val() != "none") {
+    if($("#options").val() != "none") { // NOTE: not working
       $(this).hide();
       $("#options").hide();
       $("#player").show();
@@ -24,6 +24,6 @@ $(document).ready(function() {
       alert("You haven't selected a theme")
     }
   });
-  
-  
+
+
 });
