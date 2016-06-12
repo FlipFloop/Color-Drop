064b175d2e17ca657f06aa7293d200eeda3aaa19
Pablo Gamito <pablogamito@gmail.com>
2016-06-12 19:19:57 +0200

    Adds Block at Random Position
    Made it so when game starts blocks of random color will spawn in the
    #game div at random position from left, but stuck to top. And made some
    minor code changes to make it slightly more efficient.

--- script.js
+++ script.js
@@ -1,54 +1,80 @@
 $(document).ready(function() {
 
+	var $game = $("#game")
+	var $player = $("#player")
+
   $('#options').change(function(){ // NOTE: all themes have capital letters on colors
     if($(this).val() === 'Dark'){
-      $("#game").css("background-color", "#000");
-      $("#player").css({
-        "background-color": "#FFFFFF" // using CSS function in case you want to add other stuff
-      });
+    	$game.removeClass(); //removes all classes
+    	$game.addClass("dark"); //changes css theme
     }
     if($(this).val() === 'Light'){
-      $("#game").css("background-color", "#FFFFFF");
-      $("#player").css({
-        "background-color": "#000" // using CSS function in case you want to add other stuff
-      });
+    	$game.removeClass(); //removes all classes
+    	$game.addClass("light"); //changes css theme
     }
     if($(this).val() === 'Blue_White'){
-      $("#game").css("background-color", "#0066ff");
-      $("#player").css({
-        "background-color": "#FFFFFF" // using CSS function in case you want to add other stuff
-      });
+    	$game.removeClass(); //removes all classes
+    	$game.addClass("blue_white"); //changes css theme
     }
     if($(this).val() === 'Red_White'){
-      $("#game").css("background-color", "#ff3333");
-      $("#player").css({
-        "background-color": "#FFFFFF" // using CSS function in case you want to add other stuff
-      });
+    	$game.removeClass(); //removes all classes
+    	$game.addClass("red_white"); //changes css theme
     }
   });
 
   var game = function() {
     $(document).keydown(function(event) { // keycodes: left = 37, right = 39
       if (event.which == 39 || event.which == 68) { // right arrow or D
-        if ( $("#player").position().left < $("#game").width()-$("#player").width() ) {
-		  		$("#player").css("left", "+=10");
+        if ( $player.position().left < $game.width()-$("#player").width() ) {
+		  		$player.css("left", "+=10");
 		  	}
       }
       if (event.which == 37 || event.which == 81 || event.which == 65) { // left arrow or Q on AZERTY or A on QWERTY
-        if ( $("#player").position().left > $("#player").width() - 40 ) {
-		  		$("#player").css("left", "-=10");
+        if ( $player.position().left > $("#player").width() - 40 ) {
+		  		$player.css("left", "-=10");
 		  	}
       }
     });
   };
 
+  function getRandomInt(min, max) {
+		//Returns a random integer between min (inclusive) and max (inclusive)
+		//Using Math.round() will give you a non-uniform distribution!
+	  return Math.floor(Math.random() * (max - min + 1)) + min;
+	  //Math.random return random number between 0 and 1 excluded
+	  //That * (max-min+1) will give you a number between 0 and (max-min+1) excluded
+	  //floor with turn the number into an integer (whole number) ex: 3.2 => 3 3.6 => 3
+	  //this will give whole numbers between 0 and max-min : [0;max-min] since max-min+1 was excluded
+	  //+min will give use an integer between 0+min and max-min+min=max : [min; max]
+	}
+
+	function getRandomColor(){
+		//TODO: make it check that it is not the same as the background color
+		return '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+	}
+
+  function spawn_block(){
+  	$spawned_block = $("<div class='block'></div>")
+  	$game.append($spawned_block); //add div with class block to #game div
+  	var left=getRandomInt(0, $game.width()-$spawned_block.width()); //gets a random value from left of screen where div can appear
+  	var top=getRandomInt(0, $game.height()-$spawned_block.height()); //not useful unless you don't want the div to appear at the top
+  	//adds a random position and color to spawned_block
+  	$spawned_block.css({
+  		"left": left,
+  		"background-color": getRandomColor()
+  	});
+  	//if you want a random position from top add "top" : top,
+  }
+
   $("#play").click(function() {
     if($("#options").val() != "none") {
       $(this).hide();
       $("#options").hide();
-      $("#player").show();
+      $player.show();
 
-      game();
+      game(); //start game engine
+      var spawnBlockInterval = setInterval(function(){spawn_block();}, 3000); //run spawn_block every 3000ms=3s
+      //to stop interval from running add: clearInterval(spawnBlockInterval);
 
     } else {
       alert("You haven't selected a theme")
